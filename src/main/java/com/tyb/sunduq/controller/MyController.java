7402bb043fb9d7ecdd59ca9bcf3a1cd51a037136
tayyab <tayyabkhanbhopal@gmail.com>
2025-07-08 11:42:35 +0530

    added a method

--- src/main/java/com/tyb/sunduq/controller/MyController.java
+++ src/main/java/com/tyb/sunduq/controller/MyController.java
@@ -1,7 +1,10 @@
 package com.tyb.sunduq.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class MyController {
@@ -18,5 +21,18 @@ public class MyController {
     @GetMapping("/code")
     public String code(){
         return "hello im tayyab khan";
+    }
+
+    @GetMapping("/")
+    public void redirectToUAEPass(HttpServletResponse response) throws IOException {
+        String redirectUrl = "https://stg-id.uaepass.ae/idshub/authorize" +
+                "?response_type=code" +
+                "&client_id=sandbox_stage" +
+                "&scope=urn:uae:digitalid:profile:general" +
+                "&state=HnlHOJTkTb66Y5H" +
+                "&redirect_uri=https://localhost:8000" +
+                "&acr_values=urn:safelayer:tws:policies:authentication:level:low";
+
+        response.sendRedirect(redirectUrl);
     }
 }
